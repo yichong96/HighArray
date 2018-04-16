f60282746ad01649ff66b061bbd426e08eb3bf9c
yichong96 <yichong96@gmail.com>
2018-04-16 17:11:11 +0800

    nothing

---
+++ MyCompletableFuture.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class MyCompletableFuture {
+
+
+  public static void main(String[] args) {
+
+    // List<CompletableFuture<Void>> aList =  
+    // Stream.of(1,2,3,4,5,6,7,8,9).map(element -> CompletableFuture.runAsync(() -> {
+    //   System.out.println(Thread.currentThread().getName());
+    // })).collect(Collectors.toList());
+    // System.out.println("running Async");
+    // CompletableFuture.allOf(aList.toArray(new CompletableFuture[0]));
+
+
+  //System.out.println("Start");
+
+  List<CompletableFuture<Integer>> bList = Stream.of(1,2,3,4,5,6,7,8).map(element -> CompletableFuture.supplyAsync(() -> {
+    System.out.println("Thread from first async " + element + " " +  Thread.currentThread().getName());
+    CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> {
+      System.out.println("running inner async " + element + " " + Thread.currentThread().getName());
+    });
+    System.out.println(Thread.currentThread().getName() + " " + "doing something while runAsync within async");
+    cf.join();
+    return element;
+  })).collect(Collectors.toList());
+  System.out.println("main thread run");
+   CompletableFuture.allOf(bList.toArray(new CompletableFuture[0])).join();
+  }
+
+  CompletableFuture<Void> cf1 = CompletableFuture.runAsync(() -> {
+    System.out.println("run async");
+  });
+  System.out.println("main Thread execution");
+  //cf1.join();
+}
